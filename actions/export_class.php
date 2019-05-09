6e0df20162f7197e08ee0c5017e4c3726fcb7d24
Maximilian Zinke <me@mxzinke.dev>
2019-05-09 23:10:13 +0200

    Fully functional Class-Export

--- actions/export_class.php
+++ actions/export_class.php
@@ -5,22 +5,22 @@ include('export.php');
 if (isset($_GET['class'])) {
     $class = $_GET['class'];
 } else {
-    exit();
+    echo("Wrong Classname!");
+    //exit();
+    $class = "4a";
 }
-
 
 $plain_sql = "SELECT users.username, selection.pname
             FROM users, selection, entries
-            WHERE users.uid=entries.uid AND entries.pid=selection.pid AND CHAR_LENGTH(username)>7";
+            WHERE users.uid=entries.uid AND entries.pid=selection.pid AND users.username LIKE '%_$class'";
 $db_request = mysqli_query($db, $plain_sql);
 
-if (mysqli_num_rows($db_request > 0)) {
-    while($db_result = mysqli_fetch_array($db_request)) {
-
-    }
-} else {
-    print("Error 404: There were no users found.");
+while($db_result = mysqli_fetch_array($db_request)) {
+    $data[] = array("Name" => $db_result['username'], "Projekt" => $db_result['pname']);
 }
+
+dataExport($data, $class);
+
 
 
 ?>
