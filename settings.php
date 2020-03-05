2143c0e4225c61cd030545e6ee2ca3197aa68a56
Maximilian Zinke <m.zinke@vialytics.de>
2020-03-05 17:05:23 +0100

    remove stuff

--- settings.php
+++ settings.php
@@ -2,7 +2,7 @@
 # Datenbank Login Daten
 $dbhost = "localhost";      # Hostname
 $dbuser = "mlg-wahl";       # Nutzer
-$dbpass = "derfischmax";    # Passwort
+$dbpass = "***";    # Passwort
 $dbtable = "mlg-wahl";      # Tabelle
 
 ################################################################################
